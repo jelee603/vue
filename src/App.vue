3ad4ffe787fe88650e1705cb0c54bb6c317b5396
jieun <jelee@ex-em.com>
2019-07-02 17:34:56 +0900

    컴포넌트 resize 기능추가

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <Test></Test>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Test from "./components/Test"
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+      Test
   }
 }
 </script>
