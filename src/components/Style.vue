4f8d102c42c6660982c2149bde1aea9cf6094f2e
jieun <jelee@ex-em.com>
2019-08-16 18:41:07 +0900

    [DEV-16] grid template 을 사용한 화면 추가

---
+++ src/components/Style.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="main">
+        <div class="main-top">
+            <div class="main-top-first"></div>
+            <div class="main-top-second"></div>
+        </div>
+        <div class="main-center">
+
+        </div>
+        <div class="main-bottom">
+            <div class="main-bottom-left">
+                bottom left
+            </div>
+            <div class="main-bottom-right">
+                bottom right
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "Grid",
+    }
+</script>
+<style>
+
+</style>
